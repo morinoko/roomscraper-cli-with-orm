13f2aeb1914522caacc266ef4e0b91551d2c6a43
Felice Forby <forby.1@gmail.com>
2018-06-21 08:14:55 +0900

    set up basic data scraper methods

--- Rakefile
+++ Rakefile
@@ -7,3 +7,15 @@ end
 task :console do
  Pry.start
 end
+
+task :scrape_rooms do
+	# Instantiate a scraper, and then have it find new rooms
+	columbus_scraper = RoomScraper.new('https://columbus.craigslist.org/search/roo')
+	columbus_scraper.call
+	
+# 	chillicothe_scraper = RoomScraper.new('https://chillicothe.craigslist.org/search/roo')
+# 	chillicothe_scraper.call
+	
+	
+	# After this method call, I should be able to say Room.all and have rooms there.
+end
--- lib/room_scraper.rb
+++ lib/room_scraper.rb
@@ -1,2 +1,33 @@
 class RoomScraper
+	
+	def initialize(index_url)
+		@doc = Nokogiri::HTML(open(index_url))
+	end
+		
+	def call
+		rows.each do |row_doc|
+			data = scrape_row(row_doc)
+			binding.pry
+		end	
+	end
+	
+	private
+	
+		def rows
+			# ||= if the value exists, leave it alone. 
+			# If not, assign the new value to the right
+			@rows ||= @doc.search("div.content ul.rows li.result-row")
+		end
+		
+		def scrape_row(row)
+			# scrape an individual row. return hash of attributes
+			{
+				date_created: row.search("time").attribute("datetime").text,
+				title: row.search("a.result-title").text,
+				url: row.search("a.result-title").attribute("href").value,
+				price: row.search("span.result-meta span.result-price").text
+			}
+		end
+	
+		
 end
